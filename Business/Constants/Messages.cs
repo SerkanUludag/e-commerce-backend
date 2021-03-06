083c03137213e8b9890896ab9bf3c599d98d46be
SerkanUludag <hsuluda1@gmail.com>
2021-03-06 10:49:43 +0300

    jwt implemented

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -19,5 +21,13 @@ namespace Business.Constants
         public static string ProductNameAlreadyExists = "Product name already exists";
 
         public static string CategoryLimitExceded = "There can't be more than 15 categories";
+
+        public static string AuthorizationDenied = "No authorized to do this operation";
+        public static string UserRegistered = "User registered";
+        public static string UserNotFound = "User not found";
+        public static string PasswordError = "Password Error";
+        public static string SuccessfulLogin = "Succesfully logged in";
+        public static string UserAlreadyExists = "User already exist";
+        public static string AccessTokenCreated = "Token created";
     }
 }
