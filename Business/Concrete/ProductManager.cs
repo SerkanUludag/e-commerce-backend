85ae1a99f386474629b181a6a21373a7a706b6c1
SerkanUludag <hsuluda1@gmail.com>
2021-03-14 13:28:36 +0300

    cors configuration

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -69,7 +69,7 @@ namespace Business.Concrete
         [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 23)
+            if (DateTime.Now.Hour == 22)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
